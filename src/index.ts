1996bd18afb7ec8f2a78aa58b6ccde6a91fe0528
Lobesta <kane.taka35@gmail.com>
2020-06-27 21:20:54 +0900

    applied sass middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
+import sassMiddleware from "node-sass-middleware";
+import path from "path";
 
 const expressApp = express();
 
 const PORT_NO = 3000;
+const SASS_SRC = path.join(__dirname, "../styles");  // In this case __dirname points ./src directory
+const SASS_DIST = path.join(__dirname, "../dist-styles");
+
 expressApp.set("view engine", "pug");
 expressApp.set("views", "./templates");
+
+expressApp.use(sassMiddleware({
+	src: SASS_SRC,
+    dest: SASS_DIST,
+    debug: true,
+	outputStyle: 'compressed',
+	prefix: "/static"
+})/*, express.static(path.join(__dirname, "dist-styles"))*/);
+expressApp.use(express.static(SASS_DIST));
 
 // Add routing here when you write a new pug file
 expressApp.get("", (_req, res)=>{
