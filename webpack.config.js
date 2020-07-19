04f312599f97d2e95be2ecad05ed5f0ec648d2d6
Lobesta <kane.taka35@gmail.com>
2020-07-20 06:41:44 +0900

    delete pug and scss file and slightly modified comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path') // output.pathに絶対パスを指定する必要があるため必要
+const path = require('path') // to set absolute path at output.path
 
-// Module not found: Error: Can't resolve ... の対処
+// to avoid error saying "Module not found: Error: Can't resolve ..."
 const fs = require('fs');
 let nodeModules = {};
 fs.readdirSync('node_modules')
-  .filter(function(x) {
-    return ['.bin'].indexOf(x) === -1;
-  })
-  .forEach(function(mod) {
-    nodeModules[mod] = 'commonjs ' + mod;
-  });
+	.filter(function (x) {
+		return ['.bin'].indexOf(x) === -1;
+	})
+	.forEach(function (mod) {
+		nodeModules[mod] = 'commonjs ' + mod;
+	});
 
 module.exports = {
 	mode: 'development',
